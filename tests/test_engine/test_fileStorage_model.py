ae8372da0630903a64089b6b50e1152d03098e12
Malek Salem <malek.salem.14@gmail.com>
2020-06-30 23:14:32 +0100

    update file storage test case

--- tests/test_engine/test_fileStorage_model.py
+++ tests/test_engine/test_fileStorage_model.py
@@ -14,19 +14,8 @@ class TestBaseModel(unittest.TestCase):
 
     """
 
-    def test_class_method_all(self):
-        """ Should return a dictionary datatype """
-        my_model = FileStorage()
-        holder = my_model.all()
-        self.assertEqual(type(holder), dict)
-
-    def test_class_method_new(self):
-        """ Should sets in __objects the obj with key <obj class name>.id """
-        my_model = FileStorage()
-        dummy_instance = BaseModel()
-        key = "BaseModel.{}".format(dummy_instance.id)
-        dummy_instance.name = "testing"
-        dummy_instance.number = 100
-        my_model.new(dummy_instance)
-
-        self.assertEqual(my_model.all()[key].name, "testing")
+    def test_storage_obj(self):
+        """ <storage.all()> should be object """
+        storage = FileStorage()
+        for v in storage.all().values():
+            self.assertTrue(v != dict)
