587157405339dd0dc30a16abf375a2ff8a313779
Malek Salem <malek.salem.14@gmail.com>
2020-07-01 21:25:34 +0100

    update unittests

--- tests/test_models/test_engine/test_fileStorage_model.py
+++ tests/test_models/test_engine/test_fileStorage_model.py
@@ -14,64 +14,58 @@ from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    """
-    test FileStorage
+    """ Unittests"""
 
-    """
+    @classmethod
+    def setUp(cls):
+        """steup class"""
+        cls.storage = FileStorage()
+        cls.dummy_instance = BaseModel()
 
-    def setUp(self):
-        """ store FileStorage.objects & FileStorage.file_path """
-        self.objects = FileStorage._FileStorage__objects
-        self.file_path = FileStorage._FileStorage__file_path
+    def test_instance(self):
+        """ instance type should be FileStorage"""
+        self.assertIsInstance(self.storage, FileStorage)
+        self.assertEqual(type(self.storage), FileStorage)
 
-    def test_object(self):
-        """ test self.objects type """
-        self.assertTrue(isinstance(self.objects, dict))
-
-    def test_file_path(self):
-        """ test type of file path """
-        self.assertTrue(isinstance(self.file_path, str))
-
-    def test_storage_type(self):
-        """ <storage.all()> should be dictionary """
-        storage = FileStorage()
-        database = storage.all()
-        self.assertEqual(type(database), dict)
-
-    def test_method_new(self):
-        """ test new method """
-        storage = FileStorage()
-        len_storage = len(storage.all())
-        dummy_instance = BaseModel()
-        storage.new(dummy_instance)
-        self.assertTrue(len(storage.all()) == len_storage + 1)
-
-    def test_method_reload(self):
-        """ test reload method """
-        storage = FileStorage()
-        storage.reload()
-        self.assertEqual(type(storage.all()), dict)
+    def test_method_save(self):
+        """test FileStorage save method"""
+        self.storage.new(self.dummy_instance)
+        self.storage.save()
+        self.assertIsInstance(self.storage._FileStorage__objects, dict)
 
     def test_method_all(self):
-        """ test all method """
-        storage = FileStorage()
+        """storage.all() should be dict"""
+        store = self.storage.all()
+        self.assertIsInstance(store, dict)
 
-        self.assertEqual(type(storage.all()), dict)
+    def test_method_new(self):
+        """ test FileStorage new method """
+        key = "BaseModel.{}".format(self.dummy_instance.id)
+        self.storage.new(self.dummy_instance)
+        self.assertEqual(self.dummy_instance, self.storage.all()[key])
 
+    def test_method_realod(self):
+        """ test FileStorage reload method"""
+        self.assertFalse(self.storage.reload())
+        os.remove("file.json")
+        self.assertRaises(FileNotFoundError, self.storage.reload())
 
-class TestFileStorageBaseModel(unittest.TestCase):
-    """ Test functionality of FileStorage in BaseModel """
+    def test_FileStorage_doc(self):
+        """test for docstrings"""
+        self.assertIsNotNone(FileStorage.__doc__)
 
-    def setUp(self):
-        """ setup BaseModel instance """
-        self.storage = FileStorage()
-        self.my_model = BaseModel()
-        self.my_model.save()
+    def test_FileStorage_methods_doc(self):
+        """test for docstrings"""
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
 
-    def test_base_model_update(self):
-        """ BaseModel object shoud be added to FS objects """
-        self.assertIn("BaseModel.{}".format(
-            self.my_model.id), self.storage.all().keys())
+    @classmethod
+    def tearDown(cls):
+        """clear everything"""
+        del cls.storage
+        del cls.dummy_instance
 
 if __name__ == '__main__':
     unittest.main()
