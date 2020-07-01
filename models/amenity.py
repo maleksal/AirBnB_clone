2c0b151b0e9f121a1329f40cf8b2f851fac689ed
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-07-01 23:18:38 +0100

    fix pep8

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Amenity Module """
 from models.base_model import BaseModel
+
 class Amenity(BaseModel):
     """ Amenity Class """
     name = ""
--- models/reviews.py
+++ models/reviews.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Reviews Module """
 from models.base_model import BaseModel
+
 class Reviews(BaseModel):
     """ Reviews Class """
     place_id = ""
